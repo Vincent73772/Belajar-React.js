924c2dac3080d0a9840b0d9ddb1df21c37bd5a8d
vincent73772 <vincentkurniawan737@gmail.com>
2020-06-14 16:34:41 +0700

    Berhasil React Router Dengan Passing Data

--- src/Component/Fungsional/DetailComp.jsx
+++ src/Component/Fungsional/DetailComp.jsx
@@ -6,7 +6,7 @@ function DetailComp(props) {
         <div>
         <Jumbotron>
           <h1 className="display-3">{props.location.state.judul}</h1>
-          <p className="lead">{props..state.tanggal}</p>
+          <p className="lead">{props.location.state.tanggal}</p>
           <hr className="my-2" />
           <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
           <p className="lead">
